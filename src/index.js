e4f4fcdb4795dbadeeeeee1de7ba766fa2f2a4dd
kosandrey <36664836+kosandrey@users.noreply.github.com>
2018-02-22 02:14:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,34 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var lngth = preferences.length;
+  var was_checked =[];
+  for(var i = 0; i < lngth; i++)
+  {
+    was_checked[i] = false;
+  }
+  var first_angle;
+  var second_angle;
+  var thirth_angle;
+  var amount = 0;
+  for(var i = 0; i < lngth; i++)
+  {
+    first_angle = preferences[i];
+    if(first_angle != i+1)
+    {
+      second_angle = preferences[first_angle - 1];
+      if(second_angle != i+1 && second_angle != first_angle)
+      {
+        thirth_angle = preferences[second_angle - 1];
+        if (thirth_angle == i+1 && was_checked[first_angle - 1] == false && was_checked[second_angle - 1] == false && was_checked[thirth_angle - 1] == false)
+        {
+          amount++;
+          was_checked[first_angle - 1] = true;
+          was_checked[second_angle - 1] = true;
+          was_checked[thirth_angle - 1] = true;
+        }
+      }
+    }
+  }
+  return amount;// your implementation
 };
